72dc759e5fb496ee77db97f18272a1301672a0a5
Shavkunov, Mykhail <Mykhail.Shavkunov.ctr@sabre.com>
2018-04-13 17:24:02 +0200

    react form interactive validation

--- react-form.js
+++ react-form.js
@@ -1,29 +1,52 @@
 class NameForm extends React.Component {
     constructor (...args) {
         super(...args)
-        this.handleSubmit = (event) => {
+        this.state = {error: this.props.getErrorMessage('')}
+        this.handleSubmit = event => {
             event.preventDefault()
-            console.log(this.inputNode.value);
-            console.log(event.target.elements.username.value)
+            const value = event.target.elements.username.value
+            const error = this.props.getErrorMessage(value)
+
+            if (error) {
+                alert(`error: ${error}`)
+            } else {
+                alert(`success: ${value}`)
+            }
+        }
+
+        this.handleChange = event => {
+            const {value} = event.target
+            this.setState({
+                error: this.props.getErrorMessage(value)
+            })
         }
     }
     
     render() {
+        const {error} = this.state
         return (
             <form onSubmit={this.handleSubmit}>
                 <label>
                     Name
                     <input type="text" 
                     name="username"
+                    onChange={this.handleChange}
                     ref={node => (this.inputNode = node)}/>
                 </label>
-                <button type="submit">Submit</button>
+                {error ? <div style={{color:'red'}}>{error}</div> : null}
+                <button type="submit" disabled={Boolean(error)}>Submit</button>
             </form>
         )
     }
 }
 
 ReactDOM.render(
-    <NameForm/>,
+    <NameForm 
+        getErrorMessage={value => {
+            if(value.length < 3) {
+                return `Value must be at least 3 chars, but it is only ${value.length}`
+            }
+            return null
+        }}/>,
     document.getElementById('root')
 )
